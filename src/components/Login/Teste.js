680a0808b5e7b2acd4d2222fc50a9ce365e62785
Eduardo Bonetti <duhbonetti@gmail.com>
2017-10-03 23:09:48 -0300

    First commit

---
+++ src/components/Login/Teste.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, Text, KeyboardAvoidingView } from 'react-native';
+import LoginForm from './LoginForm';
+
+export default class Teste extends Component{
+  render() {
+    return (
+      <View>
+          <Text>An app made for github using React Native</Text>
+        </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+});
